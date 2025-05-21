926fd22d6ad41881ff25a1ac0cb4e71484383c3a
kevindaouk <kevin94daouk@hotmail.se>
2025-05-21 11:34:42 +0200

    implementerat routing i app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Menu from "./pages/Menu";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Confirmation from "./pages/Confirmation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
